66bd9090d072d419fcdd0d5859dc1e4a753c40bb
John Moreau <johnmoreau19@gmail.com>
2023-10-29 20:35:27 -0700

    Fixed validation error message for product price range

--- john_moreau_MidTerm/Models/Product.cs
+++ john_moreau_MidTerm/Models/Product.cs
@@ -27,12 +27,12 @@ namespace john_moreau_MidTerm.Models
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Please enter a valid Price")]
-        [Range(0.01, int.MaxValue)]
+        [Range(0.01, int.MaxValue, ErrorMessage = "Please enter a valid Price greater than 0.01")]
         [Column(TypeName = "decimal(8,2)")]
         public decimal Price { get; set; } = 0.00M;
 
         [Required(ErrorMessage = "Please enter a Release Date")]
-        [Range(typeof(DateTime), "1/1/1900", "12/31/9999", ErrorMessage = "Date of birth must be after 1/1/1900.")]
+        [Range(typeof(DateTime), "1/1/1900", "12/31/9999", ErrorMessage = "Date must be after 1/1/1900")]
         //[DisplayFormat(DataFormatString = "{0:MM/dd/yyyy HH:mm:ss tt}", ApplyFormatInEditMode = true)] // SOURCE: https://www.mikesdotnetting.com/article/352/working-with-dates-and-times-in-razor-pages-forms#:~:text=Alternatively%2C%20you%20can%20use%20the%20asp-format%20attribute%20on,itself%3A%20DateTime%3A%20%3Cinput%20class%3D%22form-control%22%20asp-for%3D%22DateTime%22%20asp-format%3D%22%20%7B0%3Ayyyy-MM-ddTHH%3Amm%7D%22%20%2F%3E
         public DateTime ReleaseDate { get; set; } = DateTime.Now;
 
